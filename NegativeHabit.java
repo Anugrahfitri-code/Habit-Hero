6a0699f4393960eb520a465f1e85057e61867012
Muhammad Alif Sakti <muhammadalifsakti@gmail.com>
2025-05-14 19:20:52 +0800

    Update NegativeHabit.java
    Menambahkan index skor

--- NegativeHabit.java
+++ NegativeHabit.java
@@ -1,6 +1,22 @@
 public class NegativeHabit extends Habit {
+    public NegativeHabit(String name) {
+        super(name);
+    }
+
+    @Override
     public void markDone() {
-        System.out.println("Habit adalah negatif, skor berkurang.");
-        this.decreaseScore();
+        this.jumlahDilakukan++;
+        System.out.println(getName() + " dilakukan, ini kebiasaan buruk. Skor turun menjadi " + getScore());
+    }
+
+    @Override
+    public int getScore() {
+        int total = 0;
+        for (int i = 1; i <= jumlahDilakukan; i++) {
+            total -= 5 * i;  
+        }
+        return total;
     }
 }
+
+
